e27e8eb2526acddecbcce6fcc8e15897d66556d6
sandeep1404 <sandeep.kodam99@gmail.com>
2025-06-08 16:16:57 +0900

    updated training pipeline with sync to s3 bucket and swaggerui and dagshub

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -20,11 +20,13 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
-
-
+from networksecurity.cloud.s3_sync import S3Sync
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
+
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -65,6 +67,22 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys) from e
         
+    ## my local artificat is going to s3 bucket
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir, aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+        
+    ## local final model is going to s3 bucket
+    def sync_saved_models_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.model_dir, aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+        
     def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
         try:
             self.start_model_trainer_config = ModelTrainerConfig(self.training_pipeline_config)
@@ -85,6 +103,8 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            self.sync_artifact_dir_to_s3() # artifact dir is going to s3 bucket
+            self.sync_saved_models_dir_to_s3() # saved models dir is going to s3 bucket
             return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys) from e
